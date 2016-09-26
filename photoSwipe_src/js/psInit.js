da97f481b54811fd042a3d85a6a2dc7b9d84d834
Diogoxiang <a233894432@gmail.com>
2016-09-26 18:44:37 +0800

    1\ 增加demo

--- photoSwipe_src/js/psInit.js
+++ photoSwipe_src/js/psInit.js
@@ -1,7 +1,7 @@
-var initPhotoSwipeFromDOM = function(gallerySelector) {
+var initPhotoSwipeFromDOM = function (gallerySelector) {
     // parse slide data (url, title, size ...) from DOM elements
     // (children of gallerySelector)
-    var parseThumbnailElements = function(el) {
+    var parseThumbnailElements = function (el) {
         var thumbElements = el.childNodes,
             numNodes = thumbElements.length,
             items = [],
@@ -52,14 +52,14 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
     }
 
     // triggers when user clicks on thumbnail
-    var onThumbnailsClick = function(e) {
-        e = e || window.event
-        e.preventDefault ? e.preventDefault() : e.returnValue = false
+    var onThumbnailsClick = function (e) {
+        e = e || window.event;
+        e.preventDefault ? e.preventDefault() : e.returnValue = false;
 
         var eTarget = e.target || e.srcElement
 
         // find root element of slide
-        var clickedListItem = closest(eTarget, function(el) {
+        var clickedListItem = closest(eTarget, function (el) {
             return (el.tagName && el.tagName.toUpperCase() === 'FIGURE')
         })
 
@@ -73,7 +73,7 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
             childNodes = clickedListItem.parentNode.childNodes,
             numChildNodes = childNodes.length,
             nodeIndex = 0,
-            index
+            index;
 
         for (var i = 0; i < numChildNodes; i++) {
             if (childNodes[i].nodeType !== 1) {
@@ -95,7 +95,8 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
     }
 
     // parse picture index and gallery index from URL (#&pid=1&gid=2)
-    var photoswipeParseHash = function() {
+    // 更新 hash 值
+    var photoswipeParseHash = function () {
         var hash = window.location.hash.substring(1),
             params = {}
 
@@ -120,13 +121,14 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
         }
 
         return params
-    }
+    };
 
-    var openPhotoSwipe = function(index, galleryElement, disableAnimation, fromURL) {
+    //打开openPhoto
+    var openPhotoSwipe = function (index, galleryElement, disableAnimation, fromURL) {
         var pswpElement = document.querySelectorAll('.pswp')[0],
             gallery,
             options,
-            items
+            items;
 
         items = parseThumbnailElements(galleryElement)
 
@@ -134,17 +136,18 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
         options = {
 
             // define gallery index (for URL)
-            galleryUID: galleryElement.getAttribute('data-pswp-uid'),
+            galleryUID: galleryElement.getAttribute('data-pswp-uid'),  // 当前
 
-            getThumbBoundsFn: function(index) {
+            //缩放动画
+            getThumbBoundsFn: function (index) {
                 // See Options -> getThumbBoundsFn section of documentation for more info
                 var thumbnail = items[index].el.getElementsByTagName('img')[0], // find thumbnail
                     pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
                     rect = thumbnail.getBoundingClientRect()
 
-                return { x: rect.left, y: rect.top + pageYScroll, w: rect.width }
+                return {x: rect.left, y: rect.top + pageYScroll, w: rect.width}
             },
-            tapToClose: true
+            tapToClose: true  // 点击关闭
 
         }
 
@@ -179,7 +182,7 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
         // Pass data to PhotoSwipe and initialize it
         gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options)
         gallery.init()
-    }
+    };
 
     // loop through all gallery elements and bind events
     var galleryElements = document.querySelectorAll(gallerySelector)
@@ -194,7 +197,268 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
     if (hashData.pid && hashData.gid) {
         openPhotoSwipe(hashData.pid, galleryElements[hashData.gid - 1], true, true)
     }
-}
+};
 
-// execute above function
-initPhotoSwipeFromDOM('.my-gallery')
+//execute above function
+initPhotoSwipeFromDOM('.my-gallery');
+// 上面是 官网上的DOM  可不用..
+
+var initGallery = {
+    /**
+     * 初始化图片浏览器
+     * @param gallerySelector
+     */
+    init: function (gallerySelector) {
+        var galleryElements = document.querySelectorAll(gallerySelector)
+        //设置 data-pswp-uid
+        for (var i = 0, l = galleryElements.length; i < l; i++) {
+            galleryElements[i].setAttribute('data-pswp-uid', i + 1)
+            galleryElements[i].onclick = initGallery.onThumbnailsClick
+        }
+        // Parse URL and open gallery if it contains #&pid=3&gid=1
+        var hashData = this.photoswipeParseHash();
+        if (hashData.pid && hashData.gid) {
+            initGallery.openPhotoSwipe(hashData.pid, galleryElements[hashData.gid - 1], true, true)
+        }
+
+    },
+    /**
+     * 更新Hash
+     * @returns {{}}
+     */
+    photoswipeParseHash:function() {
+        var hash = window.location.hash.substring(1),
+            params = {}
+
+        if (hash.length < 5) {
+            return params
+        }
+
+        var vars = hash.split('&')
+        for (var i = 0; i < vars.length; i++) {
+            if (!vars[i]) {
+                continue
+            }
+            var pair = vars[i].split('=')
+            if (pair.length < 2) {
+                continue
+            }
+            params[pair[0]] = pair[1]
+        }
+
+        if (params.gid) {
+            params.gid = parseInt(params.gid, 10)
+        }
+
+        return params
+    },
+
+    /**
+     * 获取Items
+     */
+    parseThumbnailElements:function(el){
+
+        var thumbElements = el.childNodes,
+            numNodes = thumbElements.length,
+            items = [],
+            figureEl,
+            linkEl,
+            size,
+            item;
+
+        for (var i = 0; i < numNodes; i++) {
+            figureEl = thumbElements[i] // <figure> element
+
+            // include only element nodes
+            if (figureEl.nodeType !== 1) {
+                continue
+            }
+
+            linkEl = figureEl.children[0] // <a> element
+            //linkEl=$(figureEl).find('a');
+
+            size = linkEl.getAttribute('data-size').split('x') != undefined ? linkEl.getAttribute('data-size').split('x') :[100,100];
+
+            // create slide object
+            item = {
+                src: linkEl.getAttribute('href') != undefined ? linkEl.getAttribute('href'):linkEl.getAttribute('data-bigsrc'),
+                w: parseInt(size[0], 10),
+                h: parseInt(size[1], 10)
+            }
+
+            //当pic-thumb 子元素 大于1
+            if (figureEl.children.length > 1) {
+                // <figcaption> content
+                item.title=initGallery.getDes(figureEl);
+                //item.title = $(figureEl).find('.description').html();
+            }
+
+            if (linkEl.children.length > 0) {
+                // <img> thumbnail element, retrieving thumbnail url
+                item.msrc = initGallery.getBigSrc(linkEl);
+            }
+
+            item.el = figureEl // save link to element for getThumbBoundsFn
+            items.push(item)
+        }
+
+        return items
+    },
+    /**
+     * 打开PhotoSwipe
+     */
+    openPhotoSwipe:function(index, galleryElement, disableAnimation, fromURL){
+        var pswpElement = document.querySelectorAll('.pswp')[0], // 获取DOM模版
+            gallery,
+            options,
+            items;
+
+        items = initGallery.parseThumbnailElements(galleryElement)
+
+        console.log(items);
+
+        // define options (if needed)
+        options = {
+
+            // define gallery index (for URL)
+            galleryUID: galleryElement.getAttribute('data-pswp-uid'),  // 当前
+
+            //缩放动画
+            getThumbBoundsFn: function (index) {
+
+                // See Options -> getThumbBoundsFn section of documentation for more info
+                var thumbnail = initGallery.getBig(items[index].el), // 获取比较大的那个图片
+                    pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
+                    rect = thumbnail.getBoundingClientRect()
+                console.log(thumbnail);
+                return {x: rect.left, y: rect.top + pageYScroll, w: rect.width}
+            },
+            fullscreenEl:false,
+            shareEl:false,
+            tapToClose: true // 点击关闭
+
+        }
+
+        // PhotoSwipe opened from URL
+        if (fromURL) {
+            if (options.galleryPIDs) {
+                // parse real index when custom PIDs are used
+                // http://photoswipe.com/documentation/faq.html#custom-pid-in-url
+                for (var j = 0; j < items.length; j++) {
+                    if (items[j].pid == index) {
+                        options.index = j
+                        break
+                    }
+                }
+            } else {
+                // in URL indexes start from 1
+                options.index = parseInt(index, 10) - 1
+            }
+        } else {
+            options.index = parseInt(index, 10)
+        }
+
+        // exit if index not found
+        if (isNaN(options.index)) {
+            return
+        }
+
+        if (disableAnimation) {
+            options.showAnimationDuration = 0
+        }
+
+        // Pass data to PhotoSwipe and initialize it
+        gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options)
+        gallery.init()
+    },
+    /**
+     * 点击小图片
+     * @param e
+     */
+    onThumbnailsClick:function(e){
+        e = e || window.event;
+        e.preventDefault ? e.preventDefault() : e.returnValue = false;
+
+        var eTarget = e.target || e.srcElement;
+
+
+
+        // find root element of slide
+        var clickedListItem = initGallery.closest(eTarget, function (el) {
+            return (el.tagName && el.tagName.toUpperCase() === 'DIV' && el.getAttribute("data-thumb") )
+        });
+
+        if (!clickedListItem) {
+            return
+        }
+
+
+        console.log(clickedListItem);
+        // find index of clicked item by looping through all child nodes
+        // alternatively, you may define index via data- attribute
+        var clickedGallery = clickedListItem.parentNode,
+            childNodes = clickedListItem.parentNode.childNodes,
+            numChildNodes = childNodes.length,
+            nodeIndex = 0,
+            index;
+
+
+
+        for (var i = 0; i < numChildNodes; i++) {
+            if (childNodes[i].nodeType !== 1) {
+                continue
+            }
+
+            if (childNodes[i] === clickedListItem) {
+                index = nodeIndex
+                break
+            }
+            nodeIndex++
+        }
+
+        if (index >= 0) {
+            // open PhotoSwipe if valid index found
+            initGallery.openPhotoSwipe(index, clickedGallery)
+        }
+        return false
+
+    },
+    /**
+     * 关闭
+     * @param el
+     * @param fn
+     * @returns {*}
+     */
+    closest: function closest(el, fn) {
+        return el && (fn(el) ? el : this.closest(el.parentNode, fn))
+    },
+
+    /**
+     * 当有多个img标签-获取 class="big"的图片
+     * @param el
+     */
+    getBig:function getBig(el){
+        return el.getElementsByClassName('big')[0];
+        //console.log(el.getElementsByClassName('big')[0]);
+
+    },
+    /**
+     * 获取缩放时的那个小图片SRC
+     * @param el
+     */
+    getBigSrc:function(el){
+        return el.getElementsByClassName('big')[0].getAttribute('src');
+    },
+    /**
+     * 获取简介
+      * @param el
+     */
+    getDes:function getDes(el){
+        return el.getElementsByClassName('description')[0].innerHTML;
+    }
+
+
+
+
+};
+initGallery.init('.detail-pic');
